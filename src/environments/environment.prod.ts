315563085a8e071e012401b4966bb75b2291a379
crifer4 <118143159+crifer4@users.noreply.github.com>
2024-02-22 10:48:35 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,13 @@
 
 export const environment = {
   production: true,
-  url: 'https://helloworld-dimzlb26wq-uc.a.run.app',
+  url: '',
   firebase: {
-    apiKey: "AIzaSyDxb3OBR_9ikixF1ZgP3lKDfkQr6Lz4ur0",
-    authDomain: "firestore-grafica-15802.firebaseapp.com",
-    projectId: "firestore-grafica-15802",
-    storageBucket: "firestore-grafica-15802.appspot.com",
-    messagingSenderId: "1049849640670",
-    appId: "1:1049849640670:web:0991139c24779a2769f44f"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
